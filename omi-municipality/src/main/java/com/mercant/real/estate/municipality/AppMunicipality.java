2a2c92716b51fec1f69129db0d745c18cd1143ad
fabianaspeeencina <faspeeencina@gmail.com>
2024-10-10 10:44:12 +0200

    Add sysout

--- omi-municipality/src/main/java/com/mercant/real/estate/municipality/AppMunicipality.java
+++ omi-municipality/src/main/java/com/mercant/real/estate/municipality/AppMunicipality.java
@@ -2,6 +2,6 @@ package com.mercant.real.estate.municipality;
 
 public final class AppMunicipality {
     public static void main(String[] args) {
-
+        System.out.println("Municipality");
     }
 }
