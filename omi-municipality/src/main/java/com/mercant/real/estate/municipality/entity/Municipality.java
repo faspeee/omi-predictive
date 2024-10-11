c40df417103ad402dcd50a9b6238a101d87fe3ef
ASPEE ENCINA FABIAN ANDRES <fabian.aspeeencina@intesasanpaolo.com>
2024-10-11 15:32:35 +0200

    MBA-7 add id for entity municipality and old municipality
    init to track the commit with Jira using the key of task

--- omi-municipality/src/main/java/com/mercant/real/estate/municipality/entity/Municipality.java
+++ omi-municipality/src/main/java/com/mercant/real/estate/municipality/entity/Municipality.java
@@ -1,7 +1,15 @@
 package com.mercant.real.estate.municipality.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Municipality {
+    @Id
+    private Long id;
+
 }
--- omi-municipality/src/main/java/com/mercant/real/estate/municipality/entity/OldMunicipality.java
+++ omi-municipality/src/main/java/com/mercant/real/estate/municipality/entity/OldMunicipality.java
@@ -1,7 +1,15 @@
 package com.mercant.real.estate.municipality.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Entity
+@Getter
 public class OldMunicipality {
+    @Id
+    private Long id;
+
 }
